a548e95234bebe555e587e126e678f9428d8a007
AnujKatiyar661 <106858176+AnujKatiyar661@users.noreply.github.com>
2022-10-05 13:48:42 +0530

    Create PrintX.java

---
+++ PrintX.java
@@ -0,0 +1,24 @@
+public class Main
+{
+	public static void main(String[] args) {
+	//	System.out.println("Hello World");
+	int n=10;
+	for (int i=0;i<n;i++)
+	{
+	   for(int j=0;j<n;j++)
+	   {
+	  
+	   if(i==j ||i+j==n-1 )
+	   System.out.print("*");
+	   
+	   else
+	    System.out.print(" ");
+	   
+	 }
+	
+	 System.out.println();
+	
+	}
+	
+	}
+}
